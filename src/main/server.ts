6ae5f7b1ed23baa84dea7a49a83a6d9c13042ef6
Lucas Mendonça <lukito020@gmail.com>
2024-12-25 18:35:14 -0300

    feat: change log port

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,3 @@
 import app from './config/app'
 
-app.listen(3000, () => console.log('Server running at http://localhost:5050'))
+app.listen(3000, () => console.log('Server running at http://localhost:3000'))
