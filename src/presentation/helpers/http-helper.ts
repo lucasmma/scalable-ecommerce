af6fa5571d2bf8a859078f65066cb58dbd68b03a
Lucas Mendonça <lukito020@gmail.com>
2024-12-25 16:29:16 -0300

    feat: create helpers for http handle response

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,30 @@
+import { HttpResponse } from '../protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: error
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: {
+    error: 'Unauthorized'
+  }
+})
+
+export const forbidden = (): HttpResponse => ({
+  statusCode: 403,
+  body: {
+    error: 'Forbidden'
+  }
+})
