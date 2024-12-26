bb457bd8551b653e23f8e98f0534fbac7f63cd8e
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 17:25:56 -0300

    feat: add correct schema on edit endpoint of products

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -6,12 +6,13 @@ import env from '../config/env'
 import { makeProductController } from '../factories/product-factory'
 import { createProductSchema } from '../schemas/product/create-product-schema'
 import { idSchema } from '../schemas/id-schema'
+import { editProductSchema } from '../schemas/product/edit-product-schema'
 export default (router: Router): void => {
   const baseRoute = '/product'
   const jwtAdapter = new JwtAdapter(env.JWT_SECRET)
   const controller = makeProductController()
   router.post(baseRoute, adaptAuthRoute(jwtAdapter, 'ADMIN') , adaptRoute(controller, controller.createProduct, { body: createProductSchema }))
-  router.put(baseRoute + '/:id', adaptAuthRoute(jwtAdapter, 'ADMIN') , adaptRoute(controller, controller.editProduct, { param: idSchema, body: createProductSchema }))
+  router.put(baseRoute + '/:id', adaptAuthRoute(jwtAdapter, 'ADMIN') , adaptRoute(controller, controller.editProduct, { param: idSchema, body: editProductSchema }))
   router.delete(baseRoute + '/:id', adaptAuthRoute(jwtAdapter, 'ADMIN') , adaptRoute(controller, controller.deleteProduct, { param: idSchema }))
   router.get(baseRoute, adaptAuthRoute(jwtAdapter) , adaptRoute(controller, controller.getProducts))
 }
