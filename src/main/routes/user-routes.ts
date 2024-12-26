5abf123b29de67914609d71c728fc3415a4da987
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 10:47:10 -0300

    feat user user factory on user-routes

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -3,9 +3,11 @@ import { createUserSchema } from '../schemas/user/create-user-schema'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { UserController } from '../../presentation/controllers/user-controller'
 import { SchemaAdapter } from '../../infra/schema/schema-adapter'
+import { makeUserController } from '../factories/user-factory'
 
-export default (router: Router, controller: UserController): void => {
+export default (router: Router): void => {
   const baseRoute = '/user'
+  const controller = makeUserController()
 
   router.post(baseRoute, adaptRoute(controller, controller.createUser, new SchemaAdapter(createUserSchema)))
 }
