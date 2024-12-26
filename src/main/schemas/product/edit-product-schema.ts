6f4d3d0d7906930d5c31ba9f4b1c724dfa25afd0
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 17:24:47 -0300

    feat: create editProductSchema

---
+++ src/main/schemas/product/edit-product-schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+export const editProductSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  price: z.number().int().optional(),
+  categoryId: z.string().optional(),
+  stock: z.number().int().optional(),
+}).strict().refine((data) => {
+  return Object.values(data).some((value) => value !== undefined); // Check if at least one value is not undefined
+}, {
+  message: 'At least one field must be provided',
+});
