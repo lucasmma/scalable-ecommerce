d6784332125f8f6016f3ca45d415006442cb07db
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 10:34:10 -0300

    feat: add correct changes in adaptRoute to receive schemaAdapter

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import { createUserSchema } from '../schemas/user/create-user-schema'
-import { adaptZod } from '../adapters/express-zod-adapter'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { UserController } from '../../presentation/controllers/user-controller'
+import { SchemaAdapter } from '../../infra/schema/schema-adapter'
 
 export default (router: Router, controller: UserController): void => {
   const baseRoute = '/user'
 
-  router.post(baseRoute, adaptZod(createUserSchema), adaptRoute(controller, controller.createUser))
+  router.post(baseRoute, adaptRoute(controller, controller.createUser, new SchemaAdapter(createUserSchema)))
 }
