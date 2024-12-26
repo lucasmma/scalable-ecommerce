e2e1f46e29634c26f791ebf115dbd09b2668baa6
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 14:00:04 -0300

    feat: create route for products

---
+++ src/main/routes/product-routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import { adaptAuthRoute } from '../adapters/auth-route-adapter'
+import { adaptRoute } from '../adapters/controller-route-adapter'
+import { JwtAdapter } from '../../infra/auth/jwt-adapter'
+import env from '../config/env'
+import { makeProductController } from '../factories/product-factory'
+import { SchemaAdapter } from '../../infra/schema/schema-adapter'
+import { createProductSchema } from '../schemas/product/create-product-schema'
+export default (router: Router): void => {
+  const baseRoute = '/product'
+  const jwtAdapter = new JwtAdapter(env.JWT_SECRET)
+  const controller = makeProductController()
+  router.post(baseRoute, adaptAuthRoute(jwtAdapter, 'ADMIN') , adaptRoute(controller, controller.createProduct, new SchemaAdapter(createProductSchema)))
+}
