c06fc1ef84bcb009b7fd49fa1d9e4dda32639282
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 10:57:57 -0300

    feat: configure dovenv before access values

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+
 const env = {
   JWT_SECRET: process.env.JWT_SECRET ?? '',
   SECONDARY_JWT_SECRET: process.env.SECONDARY_JWT_SECRET ?? '',
