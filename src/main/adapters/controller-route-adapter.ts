a8c67075ab9140ecb1af3651540e04c6a9d2ba0d
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 16:37:52 -0300

    feat: receive the schemaMap from adaptRoute

--- src/main/adapters/controller-route-adapter.ts
+++ src/main/adapters/controller-route-adapter.ts
@@ -1,25 +1,59 @@
 import { HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { Response } from 'express'
 import { badRequest, ok, serverError } from '../../presentation/helpers/http-helper'
-import { SchemaProtocol } from '../../data/protocols/schema'
+import { z } from 'zod'
+import { SchemaAdapter } from '../../infra/schema/schema-adapter'
 
-export const adaptRoute = (controller: object, handle: (httpRequest: HttpRequest) => Promise<HttpResponse>, schema?: SchemaProtocol) => {
+type SchemaMap = {
+  param?: z.AnyZodObject | z.ZodEffects<any, any>
+  body?: z.AnyZodObject | z.ZodEffects<any, any>
+  query?: z.AnyZodObject | z.ZodEffects<any, any>
+}
+
+export const adaptRoute = (controller: object, handle: (httpRequest: HttpRequest) => Promise<HttpResponse>, schemaMap?: SchemaMap) => {
   return async (req: any, res: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body,
       auth: req.auth,
+      params: req.params,
       headers: req.headers
     }
     let httpResponse: HttpResponse = ok({})
 
-    if(schema) {
-      var schemaValidation = await schema.validate(httpRequest.body)
-      if(!schemaValidation.sucess){
-        httpResponse = badRequest(schemaValidation.error!)
-        res.status(httpResponse.statusCode).json({
-          error: httpResponse.body.message
-        })
-        return
+    if(schemaMap) {
+      if(schemaMap.param) {
+        var schemaAdapter = new SchemaAdapter(schemaMap.param)
+        var result = schemaAdapter.validate(req.params)
+        if(!result.sucess) {
+          httpResponse = badRequest(result.error ?? new Error('Invalid param'))
+          res.status(httpResponse.statusCode).json({
+            error: httpResponse.body.message
+          })
+          return
+        }
+      } 
+      if (schemaMap.body) {
+        var schemaAdapter = new SchemaAdapter(schemaMap.body)
+        var result = schemaAdapter.validate(req.body)
+        if(!result.sucess) {
+          httpResponse = badRequest(result.error ?? new Error('Invalid body params'))
+          res.status(httpResponse.statusCode).json({
+            error: httpResponse.body.message
+          })
+          return
+        }
+      } 
+      if (schemaMap.query) {
+        var schemaAdapter = new SchemaAdapter(schemaMap.query)
+        var result = schemaAdapter.validate(req.query)
+        if(!result.sucess) {
+          httpResponse = badRequest(result.error ?? new Error('Invalid query params'))
+          res.status(httpResponse.statusCode).json({
+            error: httpResponse.body.message
+          })
+          return
+        }
+
       }
     }
 
