9ada8f413b01d4428e40921f0c640582e445e153
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 13:59:02 -0300

    feat: add productRoutes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Router } from 'express'
 import userRoutes from '../routes/user-routes'
+import productRoutes from '../routes/product-routes'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api/v1', router)
   userRoutes(router)
+  productRoutes(router)
 }
