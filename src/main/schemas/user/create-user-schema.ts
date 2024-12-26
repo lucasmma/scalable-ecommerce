ecf649a61d336281ed2ff693d25a0d363622cd9d
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 11:29:47 -0300

    refactor: change document to documentNumber on userschema

--- src/main/schemas/user/create-user-schema.ts
+++ src/main/schemas/user/create-user-schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const createUserSchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  document: z.string().length(9),
+  documentNumber: z.string().length(9),
   password: z.string().min(6),
   passwordConfirmation: z.string().min(6)
 })
