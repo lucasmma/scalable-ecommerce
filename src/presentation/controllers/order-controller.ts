d96c3fc55bf0fadb772dcb32fd7fd50c2fc0c7ca
Lucas Mendonça <lukito020@gmail.com>
2024-12-26 18:31:01 -0300

    feat: add payCart, deliveryOrder, getOrders and getOrder to OrderController

--- src/presentation/controllers/order-controller.ts
+++ src/presentation/controllers/order-controller.ts
@@ -2,6 +2,8 @@ import { HttpRequest, HttpResponse } from '../protocols'
 import { ok } from '../helpers/http-helper'
 import prisma from '../../main/config/prisma'
 import { updateCartItemsSchema } from '../../main/schemas/order/update-cart-items-schema'
+import { payCartSchema } from '../../main/schemas/order/pay-cart-schema'
+import { idSchema } from '../../main/schemas/id-schema'
 
 export class OrderController {
   constructor() {
@@ -28,6 +30,8 @@ export class OrderController {
       include
     })
 
+    // find all products added to cart to calculate total and add to order
+
     if(!order) {
       order = await prisma.order.create({
         data: {
@@ -40,6 +44,89 @@ export class OrderController {
 
       ok(order)
     }
+
+    return ok(order)
+  }
+
+  async payCart (
+    request: HttpRequest<(typeof payCartSchema._output)>,
+  ): Promise<HttpResponse> {
+    const { user } = request.auth!
+    const { address } = request.body!
+    const { id } = request.params!
+
+    var order = await prisma.order.findFirst({
+      where: {
+        id
+      }
+    })
+
+    if(!order || order.total === 0) {
+      return ok({
+        message: 'Cart is empty'
+      })
+    }
+
+    order = await prisma.order.update({
+      where: {
+        id
+      },
+      data: {
+        status: 'CONFIRMED',
+      }
+    })
+
+    return ok(order)
+  }
+
+  async deliveryOrder (
+    request: HttpRequest,
+  ): Promise<HttpResponse> {
+    const { id } = request.params!
+
+    var order = await prisma.order.update({
+      where: {
+        id
+      },
+      data: {
+        status: 'DELIVERED',
+      }
+    })
+
+    return ok(order)
+  }
+
+  async getOrders (
+    request: HttpRequest,
+  ): Promise<HttpResponse> {
+    const { user } = request.auth!
+
+    var where = {}
+    if(user.role !== 'ADMIN') {
+      where = {
+        userId: user.id
+      }
+    }
+
+
+    var orders = await prisma.order.findMany(
+      {
+        where
+      }
+    )
+
+    return ok(orders)
+  }
+
+  async getOrder (
+    request: HttpRequest,
+  ): Promise<HttpResponse> {
+    const { id } = request.params!
+    var order = await prisma.order.findFirst({
+      where: {
+        id
+      }
+    })
 
     return ok(order)
   }
