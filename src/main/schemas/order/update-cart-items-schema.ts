a4c4cf36914529a6789a4f2f14404dc6fceaff23
Lucas Mendonça <lukito020@gmail.com>
2024-12-27 11:26:54 -0300

    feat: add optional paramethers on updateCartItemsSchema

--- src/main/schemas/order/update-cart-items-schema.ts
+++ src/main/schemas/order/update-cart-items-schema.ts
@@ -4,10 +4,10 @@ export const updateCartItemsSchema = z.object({
   addProducts: z.array(z.object({
     productId: z.string(),
     quantity: z.number().int(),
-  })),
-  removeProducts: z.array(z.string()),
+  })).optional(),
+  removeProducts: z.array(z.string()).optional(),
 }).strict().refine((data) => {
-  return data.addProducts.length > 0 || data.removeProducts.length > 0;
+  return (data.addProducts !== undefined && data.addProducts.length > 0) || (data.removeProducts !== undefined && data.removeProducts.length > 0);
 }, {
   message: 'At least one field must be provided',
 });
