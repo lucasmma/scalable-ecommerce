4b8119d8ef53cc9dbd896bc4cf94fc41e385e35f
Lucas Mendonça <lukito020@gmail.com>
2024-12-28 17:16:28 -0300

    feat: inject CacheAdapter on orderController

--- src/main/factories/order-factory.ts
+++ src/main/factories/order-factory.ts
@@ -1,10 +1,13 @@
 import { DbStockMethods } from '../../data/usecases/db-stock-methods'
+import { CacheAdapter } from '../../infra/cache/cache-adapter'
 import { MailSenderAdapter } from '../../infra/mail/mail-sender-adapter'
 import { OrderController } from '../../presentation/controllers/order-controller'
+import { redis } from '../config/redis'
 
 
 export function makeOrderController(): OrderController {
   const stockMethods = new DbStockMethods()
   const mailSenderAdapter = new MailSenderAdapter()
-  return new OrderController(stockMethods, mailSenderAdapter)
+  const cartCacheAdapter = new CacheAdapter(redis, 'cart')
+  return new OrderController(stockMethods, mailSenderAdapter, cartCacheAdapter)
 }
