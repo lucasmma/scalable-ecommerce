d56768ce5c4fc8b819511919c2a4a26f38192043
Lucas Mendonça <lukito020@gmail.com>
2024-12-28 14:39:02 -0300

    feat: setup rateLimiter on middlewares

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,10 +1,16 @@
 import { Express } from 'express'
 import { bodyParser, cors, contentType } from '../middlewares'
 import { logRequestInfo } from '../middlewares/log-info'
+import { adaptRateLimiterRoute } from '../adapters/rate-limiter-route-adapter'
+import { RateLimiter } from '../../infra/rate-limiter/rate-limiter-adapter'
+import { redis } from './redis'
 
 export default (app: Express): void => {
+  var rateLimiterAdapter = new RateLimiter(redis, 5, 60)
+
   app.use(bodyParser)
   app.use(cors)
   app.use(logRequestInfo)
   app.use(contentType)
+  app.use(adaptRateLimiterRoute(rateLimiterAdapter))
 }
