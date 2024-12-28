244648e3809fbf586d196048af474ab905f5ca40
Lucas Mendonça <lukito020@gmail.com>
2024-12-28 12:40:42 -0300

    feat: inject StockMethods on orderFactory

--- src/main/factories/order-factory.ts
+++ src/main/factories/order-factory.ts
@@ -1,6 +1,8 @@
+import { DbStockMethods } from '../../data/usecases/db-stock-methods'
 import { OrderController } from '../../presentation/controllers/order-controller'
 
 
 export function makeOrderController(): OrderController {
-  return new OrderController()
+  const stockMethods = new DbStockMethods()
+  return new OrderController(stockMethods)
 }
