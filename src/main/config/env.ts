5941dd7a14161c2ac50b98e3319e7c2cb87d9c61
Lucas Mendonça <lukito020@gmail.com>
2024-12-28 13:52:15 -0300

    feat: add correct nodemailer environment variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,6 +5,10 @@ dotenv.config()
 const env = {
   JWT_SECRET: process.env.JWT_SECRET ?? '',
   SECONDARY_JWT_SECRET: process.env.SECONDARY_JWT_SECRET ?? '',
+  NODEMAILER_HOST: process.env.NODEMAILER_HOST ?? '',
+  NODEMAILER_PORT: parseInt(process.env.NODEMAILER_PORT as string, 10),
+  NODEMAILER_USER: process.env.NODEMAILER_USER ?? '',
+  NODEMAILER_PASS: process.env.NODEMAILER_PASS ?? ''
 }
 
 export default env
