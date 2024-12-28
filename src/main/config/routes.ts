bff38322734c36a9fa2f65ef0b32fe0728826aba
Lucas Mendonça <lukito020@gmail.com>
2024-12-28 12:28:18 -0300

    feat: setup stockRoutes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -3,6 +3,7 @@ import userRoutes from '../routes/user-routes'
 import productRoutes from '../routes/product-routes'
 import productCategoryRoutes from '../routes/product-category-route'
 import orderRoutes from '../routes/order-routes'
+import stockRoutes from '../routes/stock-routes'
 
 export default (app: Express): void => {
   const router = Router()
@@ -11,4 +12,5 @@ export default (app: Express): void => {
   productRoutes(router)
   productCategoryRoutes(router)
   orderRoutes(router)
+  stockRoutes(router)
 }
