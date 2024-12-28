9bbf0ba024111d26f5d889bc4899fcef37954564
Lucas Mendonça <lukito020@gmail.com>
2024-12-28 15:13:04 -0300

    feat: create CacheProtocol

---
+++ src/data/protocols/cache.ts
@@ -0,0 +1,5 @@
+export interface CacheProtocol {
+  set: <T>(key: string, value: T, duration?: number) => Promise<void>
+  get: <T>(key: string) => Promise<T | null>
+  delete: (key: string) => Promise<void>
+}
