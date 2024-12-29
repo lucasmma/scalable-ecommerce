1649f7154aea3800a725565b6d654269da48c6a6
Lucas Mendonça <lukito020@gmail.com>
2024-12-29 16:01:30 -0300

    feat: adapt bodyParser to return json with error

--- src/main/middlewares/body-parser.ts
+++ src/main/middlewares/body-parser.ts
@@ -1,3 +1,11 @@
-import { json } from 'express'
+import { NextFunction, Request, Response, json } from 'express'
 
-export const bodyParser = json()
+export const bodyParser = (req: Request, res: Response, next: NextFunction): void => {
+  json()(req, res, (err) => {
+    if (err) {
+      res.status(400).json({ error: 'Invalid JSON payload' });
+      return;
+    }
+    next();
+  });
+};
