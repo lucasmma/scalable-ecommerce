449331df17b05c8f8dbdc89941c951d2cb3880b1
Lucas Mendonça <lukito020@gmail.com>
2024-12-29 16:35:36 -0300

    feat: set contentType to json

--- src/main/middlewares/content-type.ts
+++ src/main/middlewares/content-type.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const contentType = (req: Request, res: Response, next: NextFunction): void => {
+  req.headers['content-type'] = 'application/json'
   res.type('json')
   next()
 }
