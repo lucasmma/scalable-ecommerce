51325afb39f685ca4c84d5ab348dd497332f48cc
Lucas Mendonça <lukito020@gmail.com>
2024-12-29 15:11:11 -0300

    feat: inject mailer on mailerSenderAdapter on orderFactory

--- src/main/factories/order-factory.ts
+++ src/main/factories/order-factory.ts
@@ -2,12 +2,13 @@ import { DbStockMethods } from '../../data/usecases/db-stock-methods'
 import { CacheAdapter } from '../../infra/cache/cache-adapter'
 import { MailSenderAdapter } from '../../infra/mail/mail-sender-adapter'
 import { OrderController } from '../../presentation/controllers/order-controller'
+import { mailer } from '../config/mailer'
 import { redis } from '../config/redis'
 
 
 export function makeOrderController(): OrderController {
   const stockMethods = new DbStockMethods()
-  const mailSenderAdapter = new MailSenderAdapter()
+  const mailSenderAdapter = new MailSenderAdapter(mailer)
   const cartCacheAdapter = new CacheAdapter(redis, 'cart')
   return new OrderController(stockMethods, mailSenderAdapter, cartCacheAdapter)
 }
