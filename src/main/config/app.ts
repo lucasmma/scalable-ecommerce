3ceccf92adb0bd4bad97110df4539c551ea9750f
Lucas Mendonça <lukito020@gmail.com>
2024-12-30 12:43:21 -0300

    feat: setupSwaggerRoute before middlewares in another Router

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,13 @@
-import express from 'express'
+import express, { Router } from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupSwaggerRoute from '../routes/swagger-route'
 
 const app = express()
-setupMiddlewares(app)
-setupRoutes(app)
+setupSwaggerRoute(app)
+const router = Router()
+app.use('/api/v1', router)
+setupMiddlewares(router)
+setupRoutes(router)
 
 export default app
