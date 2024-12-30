eae4c5caf8684fb945dad1da9a746b9b74a48f49
Lucas Mendonça <lukito020@gmail.com>
2024-12-30 17:02:43 -0300

    fix: end function from prometheus should be called inside finish function

--- src/main/middlewares/log-info.ts
+++ src/main/middlewares/log-info.ts
@@ -7,11 +7,11 @@ export const logRequestInfo = (req: Request, res: Response, next: NextFunction):
   res.on('finish', () => {
     const totalTime = new Date().getTime() - startTime.getTime()
     console.log(`\x1b[1m[${new Date().toLocaleString()}] ${req.method} -> ${req.originalUrl}: \x1b[${res.statusCode >= 200 && res.statusCode < 300 ? 32 : 31}m${res.statusCode}\x1b[0m \x1b[1m(in ${totalTime}ms)\x1b[0m`)
-  })
-  end({
-    method: req.method,
-    status_code: res.statusCode,
-    route: req.originalUrl
+    end({
+      method: req.method,
+      status_code: res.statusCode,
+      route: req.originalUrl
+    })
   })
   next()
 }
