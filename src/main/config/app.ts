4bbdaf3b5e4427411ada84e0630b2adfb1c22636
Lucas Mendonça <lukito020@gmail.com>
2024-12-30 17:00:13 -0300

    feat: setup Metrics route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,9 +2,11 @@ import express, { Router } from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 import setupSwaggerRoute from '../routes/swagger-route'
+import setupMetricsRoute from '../routes/metrics-route'
 
 const app = express()
 setupSwaggerRoute(app)
+setupMetricsRoute(app)
 const router = Router()
 app.use('/api/v1', router)
 setupMiddlewares(router)
