3efe7546bb260d8893c5beb8f1c4ce4fe59e6cab
Lucas Mendonça <lukito020@gmail.com>
2024-12-30 12:51:59 -0300

    docs: create EditProductCategory on swagger

--- src/main/schemas/product-category/edit-product-category-schema.ts
+++ src/main/schemas/product-category/edit-product-category-schema.ts
@@ -1,5 +1,16 @@
 import { z } from 'zod'
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     EditProductCategory:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           example: "Electronics"
+ */
 
 export const editProductCategorySchema = z.object({
   name: z.string(),
