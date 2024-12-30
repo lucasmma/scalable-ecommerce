53a0d7a8a1ab1c4bf230bc0c7b642dc607bdf817
Lucas Mendonça <lukito020@gmail.com>
2024-12-30 18:45:04 -0300

    feat: increment appRestartCounter

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,5 @@
 import app from './config/app'
+import { appRestartsCounter } from './config/registry-metrics'
 
+appRestartsCounter.inc()
 app.listen(3000, () => console.log('Server running at http://localhost:3000'))
