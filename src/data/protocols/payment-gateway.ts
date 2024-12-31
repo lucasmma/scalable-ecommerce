0d8ee08355566359ae63786e091982fa7008c019
Lucas Mendonça <lukito020@gmail.com>
2024-12-31 12:48:42 -0300

    feat: create interface for paymentGateway

---
+++ src/data/protocols/payment-gateway.ts
@@ -0,0 +1,6 @@
+interface PaymentGateway {
+  initializePayment(amount: number, currency: string, metadata?: Record<string, any>): Promise<string>;
+  capturePayment(paymentId: string): Promise<boolean>;
+  refundPayment(paymentId: string, amount?: number): Promise<boolean>;
+  cancelPayment(paymentId: string): Promise<boolean>;
+}
