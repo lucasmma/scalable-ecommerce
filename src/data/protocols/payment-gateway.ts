4ae6fc4a1ef192dd6e566d323d88ab0090ad4f30
Lucas Mendonça <lukito020@gmail.com>
2024-12-31 12:49:30 -0300

    feat: add export and change name

--- src/data/protocols/payment-gateway.ts
+++ src/data/protocols/payment-gateway.ts
@@ -1,4 +1,4 @@
-interface PaymentGateway {
+export interface PaymentGatewayProtocol {
   initializePayment(amount: number, currency: string, metadata?: Record<string, any>): Promise<string>;
   capturePayment(paymentId: string): Promise<boolean>;
   refundPayment(paymentId: string, amount?: number): Promise<boolean>;
