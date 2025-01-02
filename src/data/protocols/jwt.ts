b5517dbeed68de92ba0742fd7b3b07aab753fbd2
Lucas Mendonça <lukito020@gmail.com>
2025-01-02 13:33:53 -0300

    refactor: the return of jwt can be null

--- src/data/protocols/jwt.ts
+++ src/data/protocols/jwt.ts
@@ -2,6 +2,6 @@ import { User } from '@prisma/client'
 
 export interface JwtProtocol {
   encode: (user: Omit<User, 'password'>, expiresIn: '8h' | '7d') => string
-  validate: (token: string) => Omit<User, 'password'>
+  validate: (token: string) => Omit<User, 'password'> | null
   decode: (token: string) => Omit<User, 'password'>
 }
